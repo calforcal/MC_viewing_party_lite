4401f1f77caadbd2f230fe879d794e8d51313392
Michael Callahan <mcalla242@gmail.com>
2023-07-17 14:24:52 -0600

    Add password requirement for creating new user

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 
 RSpec.describe '/', type: :feature do
   describe 'landing page' do
-    let!(:user1) { User.create!(name: 'Michael', email: 'mcalla123@gmail.com') }
-    let!(:user2) { User.create!(name: 'Garrett', email: 'garrett123@gmail.com') }
+    let!(:user1) { User.create!(name: 'Michael', email: 'mcalla123@gmail.com', password: "test123", password_confirmation: "test123") }
+    let!(:user2) { User.create!(name: 'Garrett', email: 'garrett123@gmail.com', password: "test123", password_confirmation: "test123") }
 
     before(:each) do
       visit '/'
